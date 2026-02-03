b5ecbd81eeba914263ee7a49c5dc94f4571ba278
jamesstilwell <jamesstilwelll@icloud.com>
2026-02-04 06:29:18 +1300

    Added in edit modes for deck and folder

--- flashcards-ui/src/components/EditPanel.jsx
+++ flashcards-ui/src/components/EditPanel.jsx
@@ -1,5 +1,8 @@
+import CreateDeckOrFolder from "./CreateDeckOrFolder";
 import CreateFlashCard from "./CreateFlashCard";
 
 export default function EditPanel({ item, type, onCardEdited}) {
     if(type == "flashcard") return <CreateFlashCard initialData={item} onSubmit={onCardEdited}/>;
+    if(type == "deck") return <CreateDeckOrFolder initialData={item} type="deck" onSubmit={onCardEdited}/>;
+    if(type == "folder") return <CreateDeckOrFolder initialData={item} type="folder" onSubmit={onCardEdited}/>;
 }
