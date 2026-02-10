b77d9848cbfcfcc7ed0730a497520366b5904ae3
jamesstilwell <jamesstilwelll@icloud.com>
2026-02-11 02:16:34 +1300

    Removed create and edit buttons and added css

--- flashcards-ui/src/components/PreviewPanel.jsx
+++ flashcards-ui/src/components/PreviewPanel.jsx
@@ -13,9 +13,11 @@ export default function PreviewPanel( {item} ) {
     }   
     const cramModeUrl = type === "root" ? '/explorer/cram/root/0' : `/explorer/cram/${type}/${item.id}`;
 
+    /*
     function handleCreate() {
         navigate(`/explorer/create/deck/${id}`, {replace : true});
     }
+    */
 
     function handleEdit() {
         navigate(`/explorer/edit/${type}/${id}`, {replace : true});
@@ -26,12 +28,10 @@ export default function PreviewPanel( {item} ) {
     }
 
     return (
-        <div>
-            <h2>{type !== "root" ? item.name : ''}</h2>
+        <div className="m-2 flex-1 flex flex-col items-center h-full">
+            <h2 className="self-start mb-2 mt-2 ml-1 overflow-hidden whitespace-nowrap w-19/20 cursor-pointer" onClick={ handleEdit}>{type !== "root" ? item.name : ''}</h2>
             
-            <button onClick={ handlePlay }>Play</button>
-            {type === "deck" && <button onClick={ handleCreate }>Create</button>}
-            {type !== "root" &&<button onClick={ handleEdit }>Edit</button>}
+            <button className="bg-amber-500 w-4/10 m-2" onClick={ handlePlay }>Play</button>
         </div>
     );
 }
