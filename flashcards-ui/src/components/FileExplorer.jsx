0c44a946ca2b0c1b01f91314517c0f34e2f76bfb
jamesstilwell <jamesstilwelll@icloud.com>
2026-02-13 13:49:17 +1300

    Tag Mode done with list working and tag delete

--- flashcards-ui/src/components/FileExplorer.jsx
+++ flashcards-ui/src/components/FileExplorer.jsx
@@ -7,18 +7,18 @@ import { TbCards } from "react-icons/tb";
 import { FaPlus } from "react-icons/fa";
 import { IoMdPricetags } from "react-icons/io";
 import { HiX } from "react-icons/hi";
+import { TbTag } from "react-icons/tb";
 
 export default function FileExplorer( {refreshKey, onCreate} ) {
     const { type, id,mode } = useParams();
     const [content,setContent] = useState([]);
-    //const [tags, setTags] = useState([]);
+    const [tags, setTags] = useState([]);
     const [isCreate, setIsCreate] = useState(false);
     const [createType, setCreateType] = useState(null);
     const [showDropdown, setShowDropdown] = useState(false);
-    
-    //const [showTagDropdown, setShowTagDropdown] = useState(false);
-    let fetchContentsUrl = type === "deck" ? `http://localhost:8080/api/decks/${id}/flashcards` : (type === "root" ? `http://localhost:8080/api/content` : `http://localhost:8080/api/content/${id}`);
-    //let fetchTagUrl = `http://localhost:8080/api/tags`;
+    const [tagMode, setTagMode] = useState(false);
+    const fetchContentsUrl = type === "deck" ? `http://localhost:8080/api/decks/${id}/flashcards` : (type === "root" ? `http://localhost:8080/api/content` : `http://localhost:8080/api/content/${id}`);
+    const fetchTagUrl = `http://localhost:8080/api/tags`;
 
     useEffect(() => {
         fetch(fetchContentsUrl, {
@@ -64,9 +64,8 @@ export default function FileExplorer( {refreshKey, onCreate} ) {
         ).catch(err => console.error("Fetch failed:", err));
     }, [fetchContentsUrl,refreshKey]); 
 
-    /** 
     useEffect(() => {
-        if(type === "deck") {
+        if(tagMode) {
         fetch(fetchTagUrl, {
             headers: {
                 'Content-Type': 'application/json',
@@ -81,9 +80,9 @@ export default function FileExplorer( {refreshKey, onCreate} ) {
                     setTags(sortedData);
             }
         ).catch(err => console.error("Tag fetch failed:", err));
-    }
-    }, [type,refreshKey,fetchTagUrl]);
-*/
+        }
+    }, [tagMode,fetchTagUrl,refreshKey]);
+
     const navigate = useNavigate();
 
     function handleClick(item) {
@@ -115,7 +114,7 @@ export default function FileExplorer( {refreshKey, onCreate} ) {
 
     function handleDelete(e,item) {
         e.stopPropagation();
-        const itemType = item.type || "flashcard";
+        const itemType = tagMode ? "tag" : item.type || "flashcard";
         const confirmed = window.confirm(`Are you sure you want to delete this ${item.type}?`);
 
         if (confirmed) {
@@ -144,27 +143,10 @@ export default function FileExplorer( {refreshKey, onCreate} ) {
         <>
             <ul className={` w-screen flex flex-col gap-4 p-4 ${mode === "cram" ? "absolute inset-0 z-50 h-full" : ""}`}>
                 <li className="flex flex-row bg-white/20 border-white/20 relative self-baseline border-2 rounded-3xl shadow-2xl shadow-black/40 transition-all hover:scale-[1.02] active:scale-95 hover:bg-slate-800/40">
-                            <button className="m-1 p-2 hover:scale-[1.05] active:scale-95" ><IoMdPricetags className="text-white" size={27}/></button>
+                            <button className="m-1 p-2 hover:scale-[1.05] active:scale-95" onClick={() => setTagMode(!tagMode)}><IoMdPricetags className="text-white" size={27}/></button>
                             <button className="m-1 p-2 hover:scale-[1.05] active:scale-95" onClick={() => setShowDropdown(!showDropdown)}>{showDropdown ? <HiX className={`text-white`} size={27} strokeWidth={1.5}/>: <FaPlus className={`text-white`} size={25}/>}</button>
                             
                             
-                            
-                           
-                            { /** type === "deck" && <button onClick={() => setShowTagDropdown(!showTagDropdown)}>v</button>}
-                            {showTagDropdown && 
-                                <ul>
-                                    <li>All Tags</li>
-                                    {tags.map((item) => (
-                                        <li>
-                                        <   h4>{item.name}</h4>
-                                        </li>
-                                    ))}
-                               </ul>
-                                
-                            }
-                            
-                        </>
-                        )*/}
                         {showDropdown &&
                               
                                     (type !== "deck" ?
@@ -193,13 +175,13 @@ export default function FileExplorer( {refreshKey, onCreate} ) {
                     </li> 
                 }
                 
-                {content.map((item) => (
+                {(tagMode ? tags : content).map((item) => (
                     <li className="bg-white/20 border-white/20 relative p-6 flex flex-row items-center cursor-pointer border-2 rounded-3xl shadow-2xl shadow-black/40 transition-all hover:scale-[1.02] active:scale-95 h-32 hover:bg-slate-800/40" key={`${item.type}-${item.id}`} onClick={() => handleClick(item)}>
                         {item.type === "folder" && <IoFolderOpenOutline className="shrink-0 mr-4 text-white/80 drop-shadow-[0_2px_4px_rgba(0,0,0,0.5)]" size={60} />}
                         {item.type === "deck" && <TbCards className="shrink-0 mr-4 text-white/80 drop-shadow-[0_2px_4px_rgba(0,0,0,0.5)]" size={60} />}
-
+                        {tagMode && <TbTag className="scale-x-[-1] shrink-0 mr-4 text-white/80 drop-shadow-[0_2px_4px_rgba(0,0,0,0.5)]" size={60}/>}
                         <div className="flex-1 min-w-0">
-                            <h3 className={`${!item.type ? "w-66 break-words overflow-y-hidden" : "truncate"} self-baseline text-white font-bold text-lg leading-tight`}>{item.type == null ? item.question : item.name}</h3>
+                            <h3 className={`${!item.type && !tagMode ? "w-66 break-words overflow-y-hidden" : "truncate"} self-baseline text-white font-bold text-lg leading-tight`}>{(item.type == null && !tagMode) ? item.question : item.name}</h3>
                             {item.type && <p>{(item.childFoldersSize !== null) && <span className="mx-2 text-rose-600 text-sm font-bold leading-tight">{item.childFoldersSize} folder{item.childFoldersSize !== 1 ? "s" : ""}</span>} 
                             {(item.childDecksSize !== null) && <span className= "mx-2 text-cyan-300 text-sm font-bold leading-tight">{item.childDecksSize} deck{item.childDecksSize !== 1 ? "s" : ""}</span>}
                             {(item.flashcardsSize !== null) && <span className=" mx-2 text-yellow-300 text-sm font-bold leading-tight">{item.flashcardsSize} flashcard{item.flashcardsSize !== 1 ? "s" : ""}</span>}</p> }
