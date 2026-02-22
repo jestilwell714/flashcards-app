3c5603392a6091fa431a6ee3d980ae8d73f0bdf3
jamesstilwell <jamesstilwelll@icloud.com>
2026-02-22 15:54:15 +1300

    UserDetailsServiceImpl done

---
+++ flashcards-api/src/main/java/com/myflashcardsapi/flashcards_api/services/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.myflashcardsapi.flashcards_api.services.impl;
+
+import com.myflashcardsapi.flashcards_api.domain.SecurityUser;
+import com.myflashcardsapi.flashcards_api.domain.User;
+import com.myflashcardsapi.flashcards_api.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new SecurityUser(user);
+    }
+}
