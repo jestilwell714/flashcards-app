fc715ff6142f2b757d1f18463e39880b0b6549fb
jamesstilwell <jamesstilwelll@icloud.com>
2026-02-26 14:09:51 +1300

    Switche IP address as on new network

--- flashcards-ui/src/config.js
+++ flashcards-ui/src/config.js
@@ -2,4 +2,4 @@ const hostname = window.location.hostname;
 
 export const API_BASE_URL = hostname === "localhost" 
     ? "http://localhost:8080" 
-    : "http://192.168.1.150:8080";
+    : "http://192.168.8.69:8080";
