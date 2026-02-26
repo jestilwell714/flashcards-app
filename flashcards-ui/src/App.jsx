15bbce1a4540771ebcbcff913476c4de9207f18a
jamesstilwell <jamesstilwelll@icloud.com>
2026-02-26 14:12:58 +1300

    Added in login and register routes and made it so that if user isnt authenticated send them to login page

--- flashcards-ui/src/App.jsx
+++ flashcards-ui/src/App.jsx
@@ -1,17 +1,29 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Explorer from './pages/Explorer';
 import CramMode from './components/CramMode';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
+const ProtectedRoute = ({ children }) => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+};
 
 function App() {
-
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<Navigate to="/explorer/preview/root/0" replace />} />
-        <Route path="/explorer/:mode/:type/:id" element={<Explorer />}/>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/explorer/:mode/:type/:id" element={<ProtectedRoute><Explorer /></ProtectedRoute>}/>
         <Route path="/explorer/:mode/:type/:id/card/:cardId" element={<Explorer />} />
       </Routes>
     </BrowserRouter>
