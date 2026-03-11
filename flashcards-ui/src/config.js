948142809cb80be3b95769b0a1c16bc10624f32f
jamesstilwell <jamesstilwelll@icloud.com>
2026-03-11 23:53:08 +1300

    Added new ip address for backend

--- flashcards-ui/src/config.js
+++ flashcards-ui/src/config.js
@@ -2,4 +2,4 @@ const hostname = window.location.hostname;
 
 export const API_BASE_URL = hostname === "localhost" 
     ? "http://localhost:8080" 
-    : "http://192.168.8.69:8080";
+    : "http://3.104.104.114:8080";
