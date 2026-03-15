fbe3eed0e3398aeba875c3b78bba9f389125e69e
jamesstilwell <jamesstilwelll@icloud.com>
2026-03-15 19:24:55 +1300

    Made username unique and made folder name not unique

--- flashcards-api/src/main/java/com/myflashcardsapi/flashcards_api/domain/Folder.java
+++ flashcards-api/src/main/java/com/myflashcardsapi/flashcards_api/domain/Folder.java
@@ -20,7 +20,7 @@ public class Folder {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
