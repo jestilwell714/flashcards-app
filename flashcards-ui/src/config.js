bb8f6f5c17601dec28c6d2810f9a1dc9e1cc73c8
jamesstilwell <jamesstilwelll@icloud.com>
2026-03-15 23:53:48 +1300

    Changed ip for new task

--- flashcards-ui/src/config.js
+++ flashcards-ui/src/config.js
@@ -2,4 +2,4 @@ const hostname = window.location.hostname;
 
 export const API_BASE_URL = hostname === "localhost" 
     ? "http://localhost:8080" 
-    : "http://3.104.104.114:8080";
+    : "http://3.27.219.81:8080";
