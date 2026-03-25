f1f3ed7d8177736e6327a29546983271841eacad
jamesstilwell <jamesstilwelll@icloud.com>
2026-03-25 19:40:32 +1300

    Link changed to new Domain name

--- flashcards-ui/src/config.js
+++ flashcards-ui/src/config.js
@@ -1,5 +1,2 @@
-const hostname = window.location.hostname;
 
-export const API_BASE_URL = hostname === "localhost" 
-    ? "http://localhost:8080" 
-    : "http://3.27.219.81:8080";
+export const API_BASE_URL =   "https://api.cramly.xyz";
